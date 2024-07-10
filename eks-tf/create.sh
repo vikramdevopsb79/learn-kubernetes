c75189709167bed2af11d16610d8972a712a7e60
sai <kuppalasaivikram@gmail.com>
2024-07-10 11:17:58 +0530

    Expense terraform project using vpc

--- eks-tf/create.sh
+++ eks-tf/create.sh
@@ -4,3 +4,6 @@ terraform apply -auto-approve
 aws eks update-kubeconfig --name example
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
+kubectl create ns argocd
+kubectl apply -n argocd -f ../argo.yaml
+
