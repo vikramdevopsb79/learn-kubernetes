8ff70a14a36536a9874bd0f359a8e72012982fba
sai <kuppalasaivikram@gmail.com>
2024-07-17 09:28:28 +0530

    added

--- eks-tf/create.sh
+++ eks-tf/create.sh
@@ -5,5 +5,5 @@ aws eks update-kubeconfig --name example
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
 kubectl create ns argocd
-kubectl apply -n argocd -f ../argo.yaml
+#kubectl apply -n argocd -f ../argo.yaml
 
